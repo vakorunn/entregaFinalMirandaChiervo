3b62baa7f0843ae0ffd7e7a06b7f46ea8958408b
vakorunn <gitEvakOrunn@gmail.com>
2023-09-08 01:04:34 -0300

    Se modifica la forma de mostrar el listado de los productos

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,11 +1,16 @@
+import React from 'react'
+
 import Product from '../Product/Product'
-import './ProductList.css'
 
 const ProductList = ({productos}) => {
   return (
-    <div className='product-container'>
-        {productos.map(product => <Product key={product.id} {...product} />)}
-    </div>
+    <>
+      <div className="product-container">
+        {
+          productos.map(product => <Product key={product.id} {...product} />)
+        }
+      </div>
+    </>
   )
 }
 
