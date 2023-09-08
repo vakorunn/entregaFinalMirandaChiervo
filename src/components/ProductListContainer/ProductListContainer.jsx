2c8beb7f214b9a84f9fdd32a80526b7055129ba9
vakorunn <gitEvakOrunn@gmail.com>
2023-09-08 01:05:00 -0300

    Se modifica el componente para corregir errores

--- src/components/ProductListContainer/ProductListContainer.jsx
+++ src/components/ProductListContainer/ProductListContainer.jsx
@@ -1,30 +1,31 @@
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import { collection, getDocs, where, query } from 'firebase/firestore'
-import { db } from '../../services/Config'
+import React, { useEffect, useState } from 'react'
+
+import { useParams } from 'react-router-dom'
+import { collection, getDocs, where, query} from 'firebase/firestore'
+
+import db from '../../services/Config'
 import ProductList from '../ProductList/ProductList'
 
 const ProductListContainer = () => {
-    const [products, setProducts] = useState([])
-    const { idCategoria } = useParams()
+  const [products, setProducts] = useState([])
+  const {idCategory} = useParams()
 
-    useEffect(() => {
-        const myProducts = idCategoria ? query(collection(db, 'inventario'), where('idCat', '==', idCategoria)) : collection(db, 'inventario')
-        getDocs(myProducts)
-            .then(res => {
-                const nuevosProductos = res.docs.map(doc => {
-                    const data = doc.data()
-                    return { id: doc.id, ...data }
-                })
-                setProducts(nuevosProductos);
-            })
-    }, [idCategoria])
-    return (
-        <>
-            <h2 style={{ color: "blue", textAlign: "center" }}> Mis Productos </h2>
-            <ProductList productos={products} />
-        </>
-    )
+  useEffect(() => {
+    const myProducts = idCategory ? query(collection(db, 'products'), where('idCategory', '==', idCategory)) : collection(db, 'products')
+    getDocs(myProducts).then(res => {
+      const newProducts = res.docs.map(doc => {
+        const data = doc.data()
+        return {id: doc.id, ...data}
+      })
+      setProducts(newProducts)
+    })
+  }, [idCategory])
+  return (
+    <>
+      <h2>Mis Productos</h2>
+      <ProductList productos={products} />
+    </>
+  )
 }
 
 export default ProductListContainer
