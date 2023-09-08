f4e8a298163638e6ec8b990b87327bdafea98f52
vakorunn <gitEvakOrunn@gmail.com>
2023-09-08 01:03:17 -0300

    Se actualizan las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import { CarritoProvider } from "./context/CartContext"
+import React from 'react'
 
-import Navbar from "./components/Navbar/Navbar"
-import ProductListContainer from "./components/ProductListContainer/ProductListContainer"
-import Cart from "./components/Cart/Cart"
-import CheckOut from "./components/Checkout/Checkout"
-import ProductDetailContainer from "./components/ProductDetail/ProductDetail"
+import Navbar from './components/Navbar/Navbar'
+import ProductListContainer from './components/ProductListContainer/ProductListContainer'
+import ProductDetailContainer from './components/ProductDetailContainer/ProductDetailContainer'
+import Cart from './components/Cart/Cart'
+import Checkout from './components/Checkout/Checkout'
+import Product from './components/Product/Product'
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const App = () => {
-  document.title = 'Belleza Oculta'
   return (
     <>
-    
       <BrowserRouter>
-        <CarritoProvider>
-          <Navbar>
-            <Routes>
-              <Route path="/" element={<ProductListContainer />} />
-              <Route path="/categoria/:idCategoria" element={ProductListContainer} />
-              <Route path="/product/:idProduct" element={<ProductDetailContainer/>} />
-              <Route path="/cart" element={<Cart/>} />
-              <Route path="/checkout" element={<CheckOut/>} /> 
-            </Routes>
-          </Navbar>
-        </CarritoProvider>
+        <Navbar>
+          <Routes>
+            <Route path='/' element={<ProductListContainer />}></Route>
+            <Route path='/categoria/:idCategoria' element={<ProductListContainer />}></Route>
+            <Route path='/product/:idProducto' element={<ProductDetailContainer />}></Route>
+            <Route path='/cart' element={<Cart />}></Route>
+            <Route path='/checkout' element={<Checkout />}></Route>
+          </Routes>
+        </Navbar>
       </BrowserRouter>
     </>
   )
