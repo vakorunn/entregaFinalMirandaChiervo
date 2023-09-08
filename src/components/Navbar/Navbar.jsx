ce1762bf0bce1e9ed352497ca7397a5569e0d248
vakorunn <gitEvakOrunn@gmail.com>
2023-09-08 01:03:50 -0300

    Se moficia el menu de la aplicacion

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,30 +1,39 @@
-import "./Navbar.css"
+import React from 'react'
 
-import { Link, NavLink } from "react-router-dom"
-import TituloPrincipal from "../TituloPrincipal/TituloPrincipal"
-import CartWidget from '../CartWidget/CartWidget'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <header>
-        <Link to="/">
-            <TituloPrincipal />
-        </Link>
-        <nav>
-            <ul>
-                <li>
-                    <NavLink classname="link" to="/"> Inicio </NavLink>
-                </li>
-                <li>
-                    <NavLink classname="link" to="/categoria/1"> Ropa </NavLink>
-                </li>
-                <li>
-                    <NavLink classname="link" to="/categoria/2"> Calzado </NavLink>
-                </li>
+    <>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <h2><NavLink to={'/'} className={'navbar-brand'}>Belleza Oculta</NavLink></h2>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink to={'/'} className={'nav-link'} aria-current='page'>Inico</NavLink>
+              </li>
+              <li className="nav-item dropdown">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Categorias
+                </a>
+                <ul className="dropdown-menu">
+                  <li>
+                    <NavLink className={'dropdown-item'} to={'/categoria'}>Camisas</NavLink>
+                  </li>
+                  <li>
+                    <NavLink className={'dropdown-item'} to={'/categoria'}>Calzado</NavLink>
+                  </li>
+                </ul>
+              </li>
             </ul>
-        </nav>
-        <CartWidget />
-    </header>
+          </div>
+        </div>
+      </nav>
+    </>
   )
 }
 
